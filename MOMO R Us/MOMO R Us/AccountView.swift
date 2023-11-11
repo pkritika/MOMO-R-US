18a18197e041e5032bd33a6da449408e786fb64b
pkritika <129911259+pkritika@users.noreply.github.com>
2023-11-10 23:35:05 -0500

    Add files via upload

---
+++ MOMO R Us/MOMO R Us/AccountView.swift
@@ -0,0 +1,24 @@
+//
+//  AccountView.swift
+//  MOMO R Us
+//
+//  Created by Kritika  on 11/10/23.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        NavigationView{
+            Text("Account List")
+                .navigationTitle("ACCOUNT")
+        }
+        
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}
---
+++ MOMO R Us/MOMO R Us/MomoListView.swift
@@ -0,0 +1,24 @@
+//
+//  MomoListView.swift
+//  MOMO R Us
+//
+//  Created by Kritika  on 11/10/23.
+//
+
+import SwiftUI
+
+struct MomoListView: View {
+    var body: some View {
+        NavigationView{
+            Text("Menu List")
+                .navigationTitle("MENU")
+        }
+
+    }
+}
+
+struct MomoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MomoListView()
+    }
+}
